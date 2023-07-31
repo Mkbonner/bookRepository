5565e4ce51becfe703fe6ccb6255aa39fc1eaf36
Matthew Bonner <91755666+Mkbonner@users.noreply.github.com>
2023-07-31 09:26:06 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bookdir',
+    version='1.0.0',
+    url='https://github.com/mypackage.git',
+    author='Matthew Bonner',
+    author_email='fake_email@hotmail.com',
+    description='Describes fans of books for class',
+    packages=find_packages(),    
+    install_requires=['numpy >= 1.11.1', 'pandas >= 1.1'],
+)
